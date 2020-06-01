8a2bec1210b59ffcf8156086a0b3676158bb3706
Patrick379 <Patrick379@Aol.com>
2020-06-01 00:48:33 -0400

    Updates

---
+++ precache-manifest.2d7efe6b0332c6997df44cd68557ece8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a546238143d24fb5806826c7b1de49f",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "ef3b468af0472300acce",
+    "url": "/dad-jokes/static/css/main.6dd44159.chunk.css"
+  },
+  {
+    "revision": "0bde1a83648851e7007e",
+    "url": "/dad-jokes/static/js/2.0acc8d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dad-jokes/static/js/2.0acc8d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3b468af0472300acce",
+    "url": "/dad-jokes/static/js/main.ff31ef2f.chunk.js"
+  },
+  {
+    "revision": "6c62c91ed167010bf8c9",
+    "url": "/dad-jokes/static/js/runtime-main.949c1f6c.js"
+  }
+]);
